b3a839f4dfc49259df0cfdc35ededb4c88830d2f
MaxSoldLTD <138915305+MaxSoldLTD@users.noreply.github.com>
2024-11-10 18:53:27 +0300

    -

--- Finance-driven data/Methods/stocks_price_data.py
+++ Finance-driven data/Methods/stocks_price_data.py
@@ -56,29 +56,28 @@ def get_stock_prices(stock_lst,start_date,end_date):
     return dataframe
 
 def analyse_stock_price(stock_lst, start_date, end_date, type='IMOEX', const=-1):
-    # Добавляем к списку
+    # # Добавляем к списку
     stock_lst.append('IMOEX')
-    # Импортируем котировки цен на нефть, стоимость доллара и евро к рублю
-    brent = yf.download('BZ=F', '2010-01-01', dt.datetime.now())['Adj Close'].sort_index(ascending=False)
-    brent = brent.rename('Brent')
-    usdrub = yf.download('RUB=X', '2010-01-01', dt.datetime.now())['Adj Close'].sort_index(ascending=False)
-    usdrub.rename('USD', inplace=True)
-    eurrub = yf.download('EURRUB=X', '2010-01-01', dt.datetime.now())['Adj Close'].sort_index(ascending=False)
-    eurrub.rename('EUR', inplace=True)
-    futures_currency_data = pd.concat([brent, usdrub], axis=1)
-    futures_currency_data = pd.concat([futures_currency_data, eurrub], axis=1)
-    futures_currency_data = futures_currency_data.sort_index(ascending=False)
-    futures_currency_data.index = futures_currency_data.index.astype(str)
+    # # Импортируем котировки цен на нефть, стоимость доллара и евро к рублю
+    # brent = yf.download('BZ=F', '2010-01-01', dt.datetime.now())['Adj Close'].sort_index(ascending=False)
+    # brent = brent.rename('Brent')
+    # usdrub = yf.download('RUB=X', '2010-01-01', dt.datetime.now())['Adj Close'].sort_index(ascending=False)
+    # usdrub.rename('USD', inplace=True)
+    # eurrub = yf.download('EURRUB=X', '2010-01-01', dt.datetime.now())['Adj Close'].sort_index(ascending=False)
+    # eurrub.rename('EUR', inplace=True)
+    # futures_currency_data = pd.concat([brent, usdrub], axis=1)
+    # futures_currency_data = pd.concat([futures_currency_data, eurrub], axis=1)
+    # futures_currency_data = futures_currency_data.sort_index(ascending=False)
+    # futures_currency_data.index = futures_currency_data.index.astype(str)
 
     # С помощью функции get_stock_prices забираем данные о ценах акций
     dataframe = get_stock_prices(stock_lst, start_date, end_date)
 
     # Соединяем данные нефти и валют с осноным датафреймом
-    dataframe = pd.merge(dataframe, futures_currency_data[futures_currency_data.index.isin(list(dataframe.index))],
-                         left_index=True, right_index=True)
+    # dataframe = pd.merge(dataframe, futures_currency_data[futures_currency_data.index.isin(list(dataframe.index))],
+    #                      left_index=True, right_index=True)
 
-    # Рассчитываем корреляционные матрицы, логарифмические доходности, меру риска VAR по каждой бумаге
-    #     try:
+
     # Считаем среднюю доходность за период
     returns = dataframe[(dataframe.index >= start_date) & (dataframe.index <= end_date)]
     returns = returns.sort_index(ascending=True)
@@ -93,16 +92,15 @@ def analyse_stock_price(stock_lst, start_date, end_date, type='IMOEX', const=-1)
     mean_returns.name = 'mean_returns'
 
     # Волатильность бумаги за выбранный период
-    min_periods = 7
+    min_periods = [7, 30, 252]
     data_tickers = list(dataframe.columns)
     empty_dict = dict()
     for i in data_tickers:
-        vol = dataframe[i].rolling(min_periods).std() * np.sqrt(min_periods)
-        vol = vol.median()
-        empty_dict[i] = vol
-    volatility = pd.DataFrame.from_dict(empty_dict, orient='index')
-    volatility[0] = round(volatility[0], 2)
-    volatility = volatility.rename(columns={0: f'{min_periods}-d volatility'})
+        vol_7 = (dataframe[i].rolling(min_periods[0]).std() * np.sqrt(min_periods[0])).median()
+        vol_30 = (dataframe[i].rolling(min_periods[1]).std() * np.sqrt(min_periods[1])).median()
+        vol_252 = (dataframe[i].rolling(min_periods[2]).std() * np.sqrt(min_periods[2])).median()
+        empty_dict[i] = [vol_7, vol_30, vol_252]
+    total_volatity = pd.DataFrame(empty_dict, index=['7d volatility', '30d volatility', '252d volatility']).transpose()
 
     # Формируем датасеты с корреляцией по разным периодам
     corr = dataframe[(dataframe.index >= start_date) & (dataframe.index <= end_date)].dropna(axis=1).corr()
@@ -110,27 +108,10 @@ def analyse_stock_price(stock_lst, start_date, end_date, type='IMOEX', const=-1)
     corr = corr[[type]]
     corr = corr.rename(columns={f'{type}': f'Correl {type}'})  # Меняем название
 
-    # Ищем наибольшие корреляции с выбранном параметром Type за нужный период
-
-    # Рисуем столбчатую диаграмму для наглядной оценки корреляции
-    #         corr = corr[abs(corr[type]) >= const]
-    #         corr = corr.sort_values(by=type, ascending=True)
-
-    #         groups = list(corr.index)
-    #         fig, ax = plt.subplots(figsize=(8, 8))
-
-    #         bars = plt.barh(groups, corr[type])
-
-    #         ax.bar_label(bars)
-
-    # plt.show()
-
-    # Создаём финальный датафрейм с результатом расчётов всех параметров
-    # Удаляем лишние колонки из датафрейма
     deleting_external_values = ['EUR', 'USD', 'IMOEX', 'Brent']
 
     final = corr.join(mean_returns)  # Добавляем мед. доходность
-    final = final.join(volatility)  # Добавляем волатильность
+    final = final.join(total_volatity)  # Добавляем волатильность
 
     final = final[~final.index.isin(deleting_external_values)]
 
